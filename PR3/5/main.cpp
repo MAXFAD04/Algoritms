d66a78a189af60091572e6a110e3cf5e19e4fd9c
MAXFAD04 <125014855+MAXFAD04@users.noreply.github.com>
2023-06-01 17:03:56 +0300

    Update main.cpp

--- PR3/5/main.cpp
+++ PR3/5/main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+using namespace std;
 
+void heapify(int arr[], int size, int root) {
+    int largest = root;
+    int left = 2 * root + 1;
+    int right = 2 * root + 2;
+
+    if (left < size && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < size && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != root) {
+        swap(arr[root], arr[largest]);
+        heapify(arr, size, largest);
+    }
+}
+
+void heapsort(int arr[], int size) {
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(arr, size, i);
+
+    for (int i = size - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    heapsort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
