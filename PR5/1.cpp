666d47d574401866b9d8eb8091beb46349e690fc
MAXFAD04 <125014855+MAXFAD04@users.noreply.github.com>
2023-06-01 17:33:11 +0300

    Update 1.cpp

--- PR5/1.cpp
+++ PR5/1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
 
+std::string encrypt(const std::string& text, int key)
+{
+    std::string result = "";
+    for (const auto& c : text) {
+        if (isalpha(c)) {
+            char base = isupper(c) ? 'A' : 'a';
+            result += static_cast<char>((c - base + key) % 26 + base);
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    std::string text;
+    std::cout << "Enter text to encrypt: ";
+    std::getline(std::cin, text);
+
+    int key;
+    std::cout << "Enter encryption key: ";
+    std::cin >> key;
+
+    std::string encryptedText = encrypt(text, key);
+    std::cout << "Encrypted text: " << encryptedText << "\n";
+
+    return 0;
