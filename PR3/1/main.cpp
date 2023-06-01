f3f17d4ba1f5f65a91160f97f376694a20fddc04
MAXFAD04 <125014855+MAXFAD04@users.noreply.github.com>
2023-06-01 16:24:10 +0300

    Update main.cpp

--- PR3/1/main.cpp
+++ PR3/1/main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int smallest = i;
+    int leftChild = 2 * i + 1;
+    int rightChild = 2 * i + 2;
+
+    if (leftChild < n && arr[leftChild] < arr[smallest])
+        smallest = leftChild;
+
+    if (rightChild < n && arr[rightChild] < arr[smallest])
+        smallest = rightChild;
+
+    if (smallest != i) {
+        swap(arr[i], arr[smallest]);
+        heapify(arr, n, smallest);
+    }
+}
+
+void buildHeap(vector<int>& arr, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+}
+
+void printArray(vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main() {
+    vector<int> arr {3, 2, 4, 7, 1, 6, 5};
+    int n = arr.size();
+
+    buildHeap(arr, n);
+
+    cout << "Min-Heap: ";
+    printArray(arr);
+
+    return 0;
+}
