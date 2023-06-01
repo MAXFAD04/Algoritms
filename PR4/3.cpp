6140447eb16f64e7a9bba63d74238852fc7f598a
MAXFAD04 <125014855+MAXFAD04@users.noreply.github.com>
2023-06-01 17:27:59 +0300

    Update 3.cpp

--- PR4/3.cpp
+++ PR4/3.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
+void countChars(const std::string& str)
+{
+    std::unordered_map<char, int> charCounts;
+    
+    for (const auto& c : str) {
+        if (charCounts.find(c) == charCounts.end()) {
+            charCounts[c] = 1;
+        }
+        else {
+            charCounts[c]++;
+        }
+    }
+    
+    for (const auto& p : charCounts) {
+        std::cout << "'" << p.first << "' occurs " << p.second << " time(s)\n";
+    }
+}
+
+int main()
+{
+    std::string inputStr = "Hello, world!";
+    countChars(inputStr);
+    return 0;
+}
